279308078f015b7a0d638b52d9adc0bc31398471
lodashreload <icarusjapan@gmail.com>
2022-03-06 21:06:05 -0500

    build functions for all boxes

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
-import "./style.css";
+import './style.css'
+
+const firstListItem = document.querySelector('li')
+function handleClickSquare(event) {
+  const thingClickedOn = event.target
+  thingClickedOn.textContent = 'X'
+}
+firstListItem?.addEventListener('click', handleClickSquare)
